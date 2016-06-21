ed2fa0f5a4004ac0e2bb60ad63c8faf7707b4fc8
Mark Furland <markfurland@gmail.com>
2016-06-21 14:49:20 -0400

    Cart is prettier.
    Woo.

--- cart.js
+++ cart.js
@@ -2,15 +2,25 @@ var app = angular.module("cart", []);
 app.controller("cartControl", function($scope){
 	$scope.name;
 	$scope.price;
-	$scope.cart = [];
+	$scope.cart = [
+		{name: 'apple',price: 42},
+		{name: 'cart', price: 33},
+		{name: 'foo', price: 42.22}
+	];
+	$scope.total = 0;
+	//Just in case the cart got initialised
+	$scope.cart.forEach(function(item){
+		$scope.total += item.price;
+	});
 	$scope.createItem = function(){
 		$scope.cart.push({name: $scope.name, price: $scope.price});
-	}
-	$scope.getTotal = function(){
-		var total = 0;
-		$scope.cart.forEach(function(item){
-			total += item.price;
-		});
-		return total;
-	}
+		$scope.total += $scope.price;
+		//Clear fields
+		$scope.name="";
+		$scope.price = "";
+	};
+	$scope.remove = function(index){
+		$scope.total -= $scope.cart[index].price;
+		$scope.cart.splice(index,1);
+	};
 });

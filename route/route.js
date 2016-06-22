45a79d493bfd84724709dab44e03c3aa2aeba0d7
Mark Furland <markfurland@gmail.com>
2016-06-22 10:11:11 -0400

    Use service for data now
    Cookies no longer scattered about like crumbs.
    Can't survive refresh though.

--- route/route.js
+++ route/route.js
@@ -24,62 +24,84 @@ app.config(function($routeProvider) {
 	});
 });
 
+app.factory('userdata', ['$cookieStore', function($cookieStore) {
+	var users = [];
+//	var curUser;
+	var curUser = $cookieStore.get('user');//Curent user
+	//var users = $cookieStore.get('users');
+
+	return {
+		setCur: function(realname,quote) {
+			users[curUser] = {realname: realname, quote: quote};
+		},
+		get: function(user){
+			return users[user];
+		},
+		getCur: function(){
+			return users[curUser];
+		},
+		setUser: function(user){
+			curUser = user;
+			$cookieStore.put('user',user);
+		},
+		getUser: function(){
+			return curUser;
+		}};
+}]);
 
 app.controller('MainController', function($scope, $routeParams) {
 	$scope.name = "LandingController";
 	$scope.params = $routeParams;
 });
 
-app.controller('LandingController', ['$scope', '$cookieStore', function($scope, $cookieStore) {
-	var user;
-	if((user = $cookieStore.get("user")) != undefined){//logged in
-		$scope.message = "Win " + user;
+app.controller('LandingController', ['$scope', 'userdata', function($scope, userdata) {
+	if((userdata.getUser()) != undefined){//logged in
+		$scope.message = "Win " + userdata.getUser();
 	}else{
 		$scope.notLoggedIn = true;
 	}
 }]);
 
-app.controller('LoginController', ['$scope', '$cookieStore', '$location', function($scope, $cookieStore, $location) {
+app.controller('LoginController', ['$scope', 'userdata', '$location', function($scope, userdata, $location) {
 	$scope.name;
 	$scope.login = function(){
-		 $cookieStore.put('user',$scope.name);
-		 $location.url('/edit/' + $scope.name);
+		userdata.setUser($scope.name);
+		$location.url('/edit/' + $scope.name);
 	};
 }]);
-app.controller('EditController', ['$scope', '$cookieStore', '$location', '$routeParams',
-		function($scope, $cookieStore, $location, $routeParams) {
-	var user;
-	if((user = $cookieStore.get("user")) == undefined){//Not logged in
+app.controller('EditController', ['$scope', 'userdata', '$location', '$routeParams',
+		function($scope, userdata, $location, $routeParams) {
+	if((userdata.getUser()) == undefined){//Not logged in
 		alert("Not logged in");
 		$location.url('/');
 	}else if($routeParams.user == undefined){
-		$location.url('/edit/' + user);
-	}else if(user != $routeParams.user){
+		$location.url('/edit/' + userdata.getUser());
+	}else if(userdata.getUser() != $routeParams.user){
 		$location.url('/');
 	}else{
 		$scope.realname;
 		$scope.quote;
 
-		var data = $cookieStore.get($routeParams.user);
+		var data = userdata.getCur();
 		if(data != undefined){
 			$scope.realname = data.realname;
 			$scope.quote = data.quote;
 		}
 
 		$scope.save = function(){
-			 $cookieStore.put(user,{realname: $scope.realname,quote: $scope.quote});
-			 $location.url('/view/' + user);
+			userdata.setCur($scope.realname, $scope.quote);
+			$location.url('/view/' + userdata.getUser());
 		};
 	}
 }]);
-app.controller('ViewController', ['$scope', '$cookieStore', '$location', '$routeParams',
-		function($scope, $cookieStore, $location, $routeParams) {
-	var data = $cookieStore.get($routeParams.user);
+app.controller('ViewController', ['$scope', 'userdata', '$location', '$routeParams',
+		function($scope, userdata, $location, $routeParams) {
+	var data = userdata.getCur();
 	$scope.user = $routeParams.user;
 	$scope.realname = data.realname;
 	$scope.quote = data.quote;
 	var user;
-	$scope.showEdit = (user = $cookieStore.get("user")) != undefined && user == $routeParams.user;
+	$scope.showEdit = (user = userdata.getUser()) != undefined && user == $routeParams.user;
 	$scope.editURL = "#/edit/" + user;
 }]);
 
